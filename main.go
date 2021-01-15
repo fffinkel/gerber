b7aaebc93787f3e3a609c8089d8c1f880660fbd7
Matt Finkel <mattf@ziprecruiter.com>
2021-01-15 10:20:09 -0500

    Work in progress
    Currently prints the current day's work

--- main.go
+++ main.go
@@ -19,8 +19,9 @@ const notesDir = "/Users/mattfinkel/.notes/"
 // - total hours worked
 
 func main() {
-	fmt.Println("leatherman: notes")
-	totals := make(map[string]int)
+	fmt.Println("------------------")
+	dayTotals := make(map[string]int)
+	weekTotals := make(map[string]int)
 
 	files, err := ioutil.ReadDir(notesDir)
 	if err != nil {
@@ -64,12 +65,17 @@ func main() {
 				//fmt.Printf("%+v\n", file.Name())
 				//fmt.Printf("%+v\n", parsedDate)
 				//fmt.Printf("%+v\n", lastDate)
-				if parsedDate.Day() == lastDate.Day() {
-					fmt.Printf("The minutes diffrence for category [%s] is: %+v\n", lastCategory, minutes)
+				//fmt.Printf("%+v\n", minutes)
+				if parsedDate.Day() == lastDate.Day() && parsedDate.Month() == lastDate.Month() {
+					//fmt.Printf("The minutes diffrence for category [%s] is: %+v\n", lastCategory, minutes)
+					weekTotals[lastCategory] += int(minutes)
+
+					if parsedDate.Day() == time.Now().Day() && parsedDate.Month() == time.Now().Month() {
+						dayTotals[lastCategory] += int(minutes)
+					}
 				}
 
 				lastDate = parsedDate
-				totals[lastCategory] += int(minutes)
 				lastCategory = strings.TrimSuffix(l[1], ")")
 
 				//fmt.Println("My Date Reformatted:\t", parsedDate.Format(time.RFC822))
@@ -81,5 +87,36 @@ func main() {
 		}
 
 	}
-	fmt.Printf("totals: %+v\n", totals)
+	//fmt.Printf("weekTotals: %+v\n", weekTotals)
+	//fmt.Printf("dayTotals: %+v\n", dayTotals)
+
+	// if lastDate.Day() == time.Now().Day() {
+	// 	now := time.Now()
+	// 	currentMinutes := now.Sub(lastDate).Minutes()
+	// 	weekTotals[lastCategory] += int(currentMinutes)
+	// 	dayTotals[lastCategory] += int(currentMinutes)
+	// }
+
+	printTotals(dayTotals)
+}
+
+func printTotals(totals map[string]int) {
+	totalsString := ""
+	totalsInt := 0
+	for k, v := range totals {
+		if k != "break" {
+			hm := minToHourMin(v)
+			totalsString = totalsString + fmt.Sprintf("%s: %s\n", k, hm)
+			totalsInt += v
+		}
+	}
+	fmt.Printf("%s", totalsString)
+	fmt.Printf("Total: %s\n", minToHourMin(totalsInt))
+	fmt.Println("------------------")
+}
+
+func minToHourMin(m int) string {
+	minutes := m % 60
+	hours := m / 60
+	return fmt.Sprintf("%dh %dm", hours, minutes)
 }
